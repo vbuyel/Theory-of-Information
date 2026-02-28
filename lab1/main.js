0c64cb7901370ce6265db6c4eeed39168386e719
vbuyel <vladosbuyel@gmail.com>
2026-02-28 18:25:13 +0300

    back to js

--- lab1/main.js
+++ lab1/main.js
@@ -1,3 +1,4 @@
+const cipher_titles = document.querySelectorAll('.cipher-title');
 const user_key = document.getElementById('user_key');
 const original_text = document.getElementById('original_text');
 const encrypted_text = document.getElementById('encrypted_text');
@@ -7,6 +8,12 @@ const btn_encrypt = document.querySelector('.action-btn-encrypt');
 const btn_decrypt = document.querySelector('.action-btn-decrypt');
 const btn_clear = document.querySelector('.action-btn-clear');
 
+cipher_titles.forEach((title) => {
+    title.addEventListener('click', () => {
+        cipher_titles.forEach((t) => t.classList.remove('active'));
+        title.classList.add('active');
+    });
+});
 
 btn_read_from_file?.addEventListener('click', () => {
     console.log('btn_read_from_file clicked');
